6a8a0b2cc1f4ce25d7b4d4cca7be7d40aaeb92d9
Vladislav Vintaykin <ge64ciw@mytum.de>
2023-06-22 23:51:18 +0200

    now you can run this from command line with arguments

--- ge_q_dts/simple_test_orthogonal.py
+++ ge_q_dts/simple_test_orthogonal.py
@@ -44,7 +44,7 @@ parser.add_argument("--learning_rate", default="auto", help="The learning rate t
 parser.add_argument("--df", default=0.9, type=float, help="The discount factor used for Q-learning")
 parser.add_argument("--eps", default=0.05, type=float, help="Epsilon parameter for the epsilon greedy Q-learning")
 parser.add_argument("--input_space", default=3, type=int, help="Number of inputs given to the agent")
-parser.add_argument("--episodes", default=50, type=int, help="Number of episodes that the agent faces in the fitness evaluation phase")
+parser.add_argument("--num_episodes", default=50, type=int, help="Number of episodes that the agent faces in the fitness evaluation phase")
 parser.add_argument("--episode_len", default=50, type=int, help="The max length of an episode in timesteps")
 parser.add_argument("--lambda_", default=50, type=int, help="Population size")
 parser.add_argument("--generations", default=100, type=int, help="Number of generations")
@@ -59,6 +59,65 @@ parser.add_argument("--low", default=-10, type=float, help="Lower bound for the 
 parser.add_argument("--up", default=10, type=float, help="Upper bound for the random initialization of the leaves")
 parser.add_argument("--types", default="#0,20,1,1;0,20,1,1;0,20,1,1", type=str, help="This string must contain the range of constants for each variable in the format '#min_0,max_0,step_0,divisor_0;...;min_n,max_n,step_n,divisor_n'. All the numbers must be integers.")
 
+
+# add arguments from common.py. they are not affecting anything, just that we can run train py with arguments from command line
+#------------------------------------------------------------------------------------------
+parser.add_argument('--tag', help='terminal tag in logger', type=str, default='')
+parser.add_argument('--alg', help='backend algorithm', type=str, default='ddpg', choices=['ddpg', 'ddpg2'])
+parser.add_argument('--learn', help='type of training method', type=str, default='normal')
+
+parser.add_argument('--env', help='gym env id', type=str, default='FetchPickAndPlace-v1')
+args, _ = parser.parse_known_args()
+if args.env == 'HandReach-v0':
+    parser.add_argument('--goal', help='method of goal generation', type=str, default='reach',
+                        choices=['vanilla', 'reach'])
+else:
+    parser.add_argument('--goal', help='method of goal generation', type=str, default='obstacle',
+                        choices=['vanilla', 'fixobj', 'interval', 'obstacle'])
+    if args.env[:5] == 'Fetch':
+        parser.add_argument('--init_offset', help='initial offset in fetch environments', type=np.float32,
+                            default=1.0)
+    elif args.env[:4] == 'Hand':
+        parser.add_argument('--init_rotation', help='initial rotation in hand environments', type=np.float32,
+                            default=0.25)
+
+parser.add_argument('--gamma', help='discount factor', type=np.float32, default=0.98)
+parser.add_argument('--clip_return', help='whether to clip return value', type=bool, default=True)
+parser.add_argument('--eps_act', help='percentage of epsilon greedy explorarion', type=np.float32, default=0.3)
+parser.add_argument('--std_act', help='standard deviation of uncorrelated gaussian explorarion', type=np.float32,
+                    default=0.2)
+
+parser.add_argument('--pi_lr', help='learning rate of policy network', type=np.float32, default=1e-3)
+parser.add_argument('--q_lr', help='learning rate of value network', type=np.float32, default=1e-3)
+parser.add_argument('--act_l2', help='quadratic penalty on actions', type=np.float32, default=1.0)
+parser.add_argument('--polyak', help='interpolation factor in polyak averaging for DDPG', type=np.float32,
+                    default=0.95)
+
+parser.add_argument('--epochs', help='number of epochs', type=np.int32, default=20)
+parser.add_argument('--cycles', help='number of cycles per epoch', type=np.int32, default=20)
+parser.add_argument('--episodes', help='number of episodes per cycle', type=np.int32, default=50)
+parser.add_argument('--timesteps', help='number of timesteps per episode', type=np.int32,
+                    default=(50 if args.env[:5] == 'Fetch' else 100))
+parser.add_argument('--train_batches', help='number of batches to train per episode', type=np.int32, default=20)
+
+parser.add_argument('--buffer_size', help='number of episodes in replay buffer', type=np.int32, default=10000)
+parser.add_argument('--buffer_type', help='type of replay buffer / whether to use Energy-Based Prioritization',
+                    type=str, default='energy', choices=['normal', 'energy'])
+parser.add_argument('--batch_size', help='size of sample batch', type=np.int32, default=256)
+parser.add_argument('--warmup', help='number of timesteps for buffer warmup', type=np.int32, default=10000)
+parser.add_argument('--her', help='type of hindsight experience replay', type=str, default='future',
+                    choices=['none', 'final', 'future'])
+parser.add_argument('--her_ratio', help='ratio of hindsight experience replay', type=np.float32, default=0.8)
+parser.add_argument('--pool_rule', help='rule of collecting achieved states', type=str, default='full',
+                    choices=['full', 'final'])
+
+parser.add_argument('--hgg_c', help='weight of initial distribution in flow learner', type=np.float32, default=3.0)
+parser.add_argument('--hgg_L', help='Lipschitz constant', type=np.float32, default=5.0)
+parser.add_argument('--hgg_pool_size', help='size of achieved trajectories pool', type=np.int32, default=1000)
+parser.add_argument('--forced_hgg_dt_step_size', help='step size between intermediate goals', type=np.float32,
+                    default=None)
+parser.add_argument('--c', help='c parameter to measure success basing on Q function', type=np.float32, default=-1)
+parser.add_argument('--save_acc', help='save successful rate', type=bool, default=True)
 
 # Setup of the logging
 
@@ -120,13 +179,13 @@ with open(logfile, "a") as f:
 
 # Definition of the fitness evaluation function
 
-def evaluate_fitness(fitness_function, leaf, genotype, grid_size, agent_start, agent_goal, dimensions, reward_type, episodes=args.episodes):
+def evaluate_fitness(fitness_function, leaf, genotype, grid_size, agent_start, agent_goal, dimensions, reward_type, episodes=args.num_episodes):
     phenotype, _ = GrammaticalEvolutionTranslator(grammar).genotype_to_str(genotype)
     bt = PythonDT(phenotype, leaf)
     return fitness_function(bt, grid_size, agent_start, agent_goal, dimensions, reward_type, episodes)
 
 
-def fitness(x, grid_size, agent_start, agent_goal, dimensions, reward_type, obstacle_is_on, episodes=args.episodes):
+def fitness(x, grid_size, agent_start, agent_goal, dimensions, reward_type, obstacle_is_on, episodes=args.num_episodes):
     random.seed(args.seed)
     np.random.seed(args.seed)
     global_cumulative_rewards = []
